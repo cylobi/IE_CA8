ba145d0f6a4ea3540b20b730fc1f34f616758994
cylobi <javad.afsari01@gmail.com>
2024-04-20 16:53:47 +0330

    initial commit: Project Initialzed

---
+++ src/main/java/org/example/ie_ca4/IeCa4Application.java
@@ -0,0 +1,13 @@
+package org.example.ie_ca4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeCa4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IeCa4Application.class, args);
+    }
+
+}
