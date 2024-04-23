b0aeb96c8e0dc3a73eb84c2220a609c42bad5e95
m.fateh <mohammad.fateh256@ut.ac.ir>
2024-04-23 16:10:53 +0330

    feat: table details

--- src/main/java/org/ie/mizdooni/controller/TableController.java
+++ src/main/java/org/ie/mizdooni/controller/TableController.java
@@ -2,13 +2,13 @@ package org.ie.mizdooni.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.ie.mizdooni.model.RestaurantModel;
 import org.ie.mizdooni.model.TableModel;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -20,6 +20,20 @@ public class TableController {
     String getAll() {
         try {
             String json = new ObjectMapper().writeValueAsString(TableModel.getAllObjects());
+            return json;
+        }
+        catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+        return "Error!";
+    }
+
+    @RequestMapping(path="/table/{restname}/{number}", method = RequestMethod.GET)
+    String getDetails(@PathVariable String restname, @PathVariable int number){
+        try {
+            String json = new ObjectMapper().writeValueAsString(
+                    TableModel.findByRestaurantNameAndNumber(restname,number)
+                );
             return json;
         }
         catch (JsonProcessingException e){
