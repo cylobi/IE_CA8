41a48372e74156eb25c6b0ff6b303101192a9a92
m.fateh <mohammad.fateh256@ut.ac.ir>
2024-04-26 07:50:52 +0330

    feat: footer added

--- frontend/views/CustomerView.tsx
+++ frontend/views/CustomerView.tsx
@@ -1,10 +1,19 @@
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 function CustomerView () {
+    const backgroundCss = {
+        backgroundColor: "#fffcfc",
+        color: "white",
+        height: "100vh",
+        width: "100%",
+        margin: 0
+    };
     return (
-    <>
+    <div style={backgroundCss}>
     <Header />
-    </>
+    <Footer />
+    </div>
     );
 }
 
