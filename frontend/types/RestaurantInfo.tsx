e163947ecde3300e37db17f43390e822635eec8b
m.fateh <mohammad.fateh256@ut.ac.ir>
2024-05-04 20:07:55 +0330

    clean : remove comments

--- frontend/types/RestaurantInfo.tsx
+++ frontend/types/RestaurantInfo.tsx
@@ -16,5 +16,3 @@ export default interface RestaurantInfo {
   overall: number;
   reviewsCount: number;
 }
-
-// {"name":"The Commoner","id":1001,"managerUsername":"ali","type":"American","startTime":"07:00","endTime":"23:00","description":"At our gastropub, we don't distinguish between commoners and kings; we just want to feed the good people of Pittsburgh. In the restaurant, seasonal menus add a modern flair to classic comforts, complemented by a robust selection of local beers and craft spirits. It's all served in an industrial-inspired setting in downtown Pittsburgh. Come and join us for an uncommonly good time.","imageUrl":"https://resizer.otstatic.com/v2/photos/xlarge/1/31676318.webp","address":{"country":"US","city":"Pittsburgh","street":"620 William Penn Place"},"startHour":7,"endHour":23,"city":"Pittsburgh"},
