e84ae011d790fa852b735f183fbb88ddd9cc30fe
m.fateh <mohammad.fateh256@ut.ac.ir>
2024-05-13 21:43:13 +0330

    style: remove additional files

--- src/main/java/org/ie/mizdooni/model/repository/GlobalDataRepository.java
+++
@@ -1,8 +0,0 @@
-package org.ie.mizdooni.model.repository;
-
-import org.ie.mizdooni.model.GlobalData;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GlobalDataRepository extends CrudRepository<GlobalData, Integer> {
-
-}
