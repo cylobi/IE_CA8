afa49329094195c1157305bbed7ba403e758378c
m.fateh <mohammad.fateh256@ut.ac.ir>
2024-05-17 15:10:31 +0330

    style: review notes and cleaning

--- src/main/java/org/ie/mizdooni/dao/TableDao.java
+++ src/main/java/org/ie/mizdooni/dao/TableDao.java
@@ -6,4 +6,28 @@ public class TableDao extends BaseDao<TableModel> {
     public TableDao() {
         super(TableModel.class);
     }
+
+    // public static List<TableModel> findByRestaurantName(String restaurantName){
+    // return allObjects.stream()
+    // .filter(model -> model.getRestaurantName().compareTo(restaurantName) == 0)
+    // .collect(Collectors.toList());
+    // }
+
+    // public static void addObject(TableModel table){
+    // allObjects.add(table);
+    // }
+
+    // public static TableModel findByRestaurantNameAndNumber(String restaurantName,
+    // int tableNumber){
+    // var allResults = allObjects.stream()
+    // .filter(
+    // model -> model.getRestaurantName().compareTo(restaurantName) == 0 &&
+    // model.getTableNumber() == tableNumber
+    // ).collect(Collectors.toList());
+    // if(allResults.isEmpty()){
+    // return null;
+    // }
+    // assert allResults.size() == 1;
+    // return allResults.get(0);
+    // }
 }
--- src/main/java/org/ie/mizdooni/model/TableModel.java
+++ src/main/java/org/ie/mizdooni/model/TableModel.java
@@ -1,23 +1,20 @@
 package org.ie.mizdooni.model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.ie.mizdooni.utils.validator.ValidatorException;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
-// @Embeddable
 class TableId implements Serializable {
     private String restaurantName;
     private Integer tableNumber;
@@ -43,8 +40,6 @@ class TableId implements Serializable {
 @Table(name = "Tables")
 @IdClass(TableId.class)
 public class TableModel extends BaseModel {
-    // private static List<TableModel> allObjects = new ArrayList<>();
-
     @Id
     @Column(name = "tableNumber", nullable = false)
     private int tableNumber;
@@ -53,7 +48,8 @@ public class TableModel extends BaseModel {
     @Column(name = "restaurantName", nullable = false)
     private String restaurantName;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY) // many times just we want to read restaurants with its tables. not reversed.
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "restaurantName", referencedColumnName = "name", insertable = false, updatable = false)
     private RestaurantModel restaurant;
 
@@ -62,6 +58,46 @@ public class TableModel extends BaseModel {
 
     @Column(name = "managerUsername")
     private String managerUsername;
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public RestaurantModel getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(RestaurantModel restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Integer getSeatsNumber() {
+        return seatsNumber;
+    }
+
+    public void setSeatsNumber(Integer seatsNumber) {
+        this.seatsNumber = seatsNumber;
+    }
+
+    public String getManagerUsername() {
+        return managerUsername;
+    }
+
+    public void setManagerUsername(String managerUsername) {
+        this.managerUsername = managerUsername;
+    }
 
     @Override
     public void validate() throws ValidatorException {
