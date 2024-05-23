365a1b260c0823205f5627cd112a8e08141669e4
m.fateh <mohammad.fateh256@ut.ac.ir>
2024-05-23 18:28:22 +0330

    feat: authentication using tutorial worked

---
+++ src/main/java/org/ie/mizdooni/token/Token.java
@@ -0,0 +1,44 @@
+package org.ie.mizdooni.token;
+
+import org.ie.mizdooni.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "token")
+public class Token {
+
+  @Id
+  @GeneratedValue
+  public Integer id;
+
+  @Column(unique = true)
+  public String token;
+
+  @Enumerated(EnumType.STRING)
+  public TokenType token_type = TokenType.BEARER;
+
+  public boolean revoked;
+
+  public boolean expired;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  public User user;
+}
---
+++ src/main/java/org/ie/mizdooni/token/TokenType.java
@@ -0,0 +1,5 @@
+package org.ie.mizdooni.token;
+
+public enum TokenType {
+  BEARER
+}
