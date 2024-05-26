41d866d7cc4a1ebdb0270a28b23bcc3383bba0dc
m.fateh <mohammad.fateh256@ut.ac.ir>
2024-05-26 23:27:26 +0330

    feat: set error code 401 for authentication errors

--- src/main/java/org/ie/mizdooni/config/JwtAuthenticationFilter.java
+++ src/main/java/org/ie/mizdooni/config/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package org.ie.mizdooni.config;
 
-import org.ie.mizdooni.token.TokenRepository;
 import org.ie.mizdooni.token.UserTokenDao;
 
 import jakarta.servlet.FilterChain;
@@ -8,13 +7,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.beans.Transient;
 import java.io.IOException;
-import java.security.Security;
-
-import jakarta.transaction.TransactionScoped;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -24,13 +19,14 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.JwtException;
+
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JwtService jwtService;
   private final UserDetailsService userDetailsService;
-  private final TokenRepository tokenRepository;
   private final UserTokenDao tokenDao;
 
   @Override
@@ -43,27 +39,41 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
     final String authHeader = request.getHeader("Authorization");
     final String jwt;
-    final String userEmail;
+    final String username;
     if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-      filterChain.doFilter(request, response);
+      response.setStatus(401);
+      response.getWriter().write("WRONG_AUTHENTICATION_HEADER_FORMAT");
       return;
     }
-    jwt = authHeader.substring(7);
-    userEmail = jwtService.extractUsername(jwt);
-    if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-      UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+    try {
+      jwt = authHeader.substring(7);
+      username = jwtService.extractUsername(jwt);
+      if (username == null || SecurityContextHolder.getContext().getAuthentication() != null) {
+        throw new JwtException("username has a problem");
+      }
+
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
       var isTokenValid = tokenDao.findByToken(jwt).map(t -> !t.isExpired() && !t.isRevoked()).orElse(false);
       if (!isTokenValid) {
-        filterChain.doFilter(request, response);
-        return;
+        throw new JwtException("token not found");
       }
+
       if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null,
             userDetails.getAuthorities());
         authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authToken);
+        filterChain.doFilter(request, response);
+        return;
       }
+
+    } catch (JwtException e) {
+      response.setStatus(401);
+      response.getWriter().write("INVALID_JWT");
+      return;
     }
-    filterChain.doFilter(request, response);
+
+    response.setStatus(401);
+    response.getWriter().write("AUTHENTICATION_ERROR");
   }
 }
--- src/main/java/org/ie/mizdooni/config/SecurityConfiguration.java
+++ src/main/java/org/ie/mizdooni/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package org.ie.mizdooni.config;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
