d8aeaccc3f2eb6c917371d2692ad157e3cdcd19f
m.fateh <mohammad.fateh256@ut.ac.ir>
2024-05-27 17:50:22 +0330

    fix : jwt restored

--- src/main/java/org/ie/mizdooni/config/JwtAuthenticationFilter.java
+++ src/main/java/org/ie/mizdooni/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package org.ie.mizdooni.config;
 
+import org.ie.mizdooni.token.TokenRepository;
 import org.ie.mizdooni.token.UserTokenDao;
 
 import jakarta.servlet.FilterChain;
@@ -7,9 +8,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.beans.Transient;
 import java.io.IOException;
-import lombok.RequiredArgsConstructor;
+import java.security.Security;
 
+import jakarta.transaction.TransactionScoped;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,14 +24,13 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import io.jsonwebtoken.JwtException;
-
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JwtService jwtService;
   private final UserDetailsService userDetailsService;
+  private final TokenRepository tokenRepository;
   private final UserTokenDao tokenDao;
 
   @Override
@@ -39,41 +43,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
     final String authHeader = request.getHeader("Authorization");
     final String jwt;
-    final String username;
+    final String userEmail;
     if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-      response.setStatus(401);
-      response.getWriter().write("WRONG_AUTHENTICATION_HEADER_FORMAT");
+      filterChain.doFilter(request, response);
       return;
     }
-    try {
-      jwt = authHeader.substring(7);
-      username = jwtService.extractUsername(jwt);
-      if (username == null || SecurityContextHolder.getContext().getAuthentication() != null) {
-        throw new JwtException("username has a problem");
-      }
-
-      UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+    jwt = authHeader.substring(7);
+    userEmail = jwtService.extractUsername(jwt);
+    if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
       var isTokenValid = tokenDao.findByToken(jwt).map(t -> !t.isExpired() && !t.isRevoked()).orElse(false);
       if (!isTokenValid) {
-        throw new JwtException("token not found");
+        filterChain.doFilter(request, response);
+        return;
       }
-
       if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null,
             userDetails.getAuthorities());
         authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authToken);
-        filterChain.doFilter(request, response);
-        return;
       }
-
-    } catch (JwtException e) {
-      response.setStatus(401);
-      response.getWriter().write("INVALID_JWT");
-      return;
     }
-
-    response.setStatus(401);
-    response.getWriter().write("AUTHENTICATION_ERROR");
+    filterChain.doFilter(request, response);
   }
 }
